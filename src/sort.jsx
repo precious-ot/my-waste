3f53218bc24d7f827abb13083c465e0bddfa10d1
USER <otusanyaprecious388@gmail.com>
2025-05-29 16:04:26 +0100

    my changes

--- src/sort.jsx
+++ src/sort.jsx
@@ -31,20 +31,21 @@ export default function Sort(){
     
     return(
         <div>
-        <div className="bg-blue-950 mt-8 grid md:flex md:justify-end md:place-items-center text-white p-8 rounded-4xl">
+            
+        <div className="relative bg-blue-950 mt-8 grid md:flex md:justify-end md:place-items-center text-white p-8 rounded-4xl">
             <h1 className="font-semibold md:me-4">Sort by</h1>
             
-            <button className="md:mt-0 mt-3 bg-blue-800 py-2 px-4 rounded-full md:w-60 text-start" onClick={Toggle}>Price: {low ? 'Low to High' : 'High to Low'}</button>
-            </div>
-            {open && (
+            <button className="relative md:mt-0 mt-3 bg-blue-800 py-2 px-4 rounded-full md:w-60 text-start" onClick={Toggle}>Price: {low ? 'Low to High' : 'High to Low'}</button>
+        {open && (
                 <div onClick={handleclick} className="absolute z-999 bg-black text-white border
-                 border-neutral-700 py-1 min-w-50 xl:left-210 xl:bottom-46 lg:bottom-64 lg:left-150 md:bottom-64 md:w-70 md:left-95 sm:left-25  sm:bottom-55 sm:w-80  rounded-xl left-14  bottom-50">
+                 border-neutral-700 mt-28 md:mt-35 py-1 min-w-50 overflow-auto" tabIndex={1}>
                     <li onClick={lowClick} className="ps-6 grid py-2 hover:bg-blue-900" key={Option}>
                         Low to High</li>
                     <li onClick={highClick} className="ps-6 py-2 grid hover:bg-blue-900" key={Option}>
                         High to Low</li>
                 </div>
             )}
+            </div>
             {low && (
                 <Low />
             )}
