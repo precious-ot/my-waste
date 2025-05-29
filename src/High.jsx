37da40e4e398bbd144df5a9ef8409111a224f9fb
USER <otusanyaprecious388@gmail.com>
2025-05-29 16:29:13 +0100

    im sorry

--- src/High.jsx
+++ src/High.jsx
@@ -29,7 +29,7 @@ function Yards({number, url, price, altText}){
             </div>
             </div>
             {skip && (
-                <div className="fixed sm:p-5 px-10 w-[100%] top-0 bg-black h-full sm:w-70 right-0">
+                <div className="fixed sm:py-8 px-6 px-10 w-[100%] top-0 bg-black h-full sm:w-70 right-0">
                    <button className="text-white font-medium flex place-items-center" onClick={skipRemove}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
