7a03615313ebfe83dbbe1d52e113feb158d33747
USER <otusanyaprecious388@gmail.com>
2025-06-03 15:48:59 +0100

    life

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import Sort from './sort';
 function App(part) {
   return (
     <div className='flex '>
+      <div id='sideBar' className='shrink-0 order-1'></div>
     <div className='bg-black px-6 sm:px-10 grow-1'>
       <Nav />
       <Heading />
       <Sort/>
     </div>
-    <div id='sideBar' className='shrink-0'></div>
+    
       </div>
 
   );
